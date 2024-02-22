4dfc814164285b3e0690419b5b04341880b47a3c
Hasan <hasanmamun113099@gmail.com>
2024-02-22 18:29:59 +0600

    first commit

---
+++ script/script.js
@@ -0,0 +1,48 @@
+const seats = document.getElementsByClassName('btn-all')
+
+
+for(const seat of seats) {
+
+   seat.addEventListener("click", function (event){
+
+    const seatName = event.target.innerText;
+
+    const ticketPrice = parseInt(550);
+
+    const ticketAll = document.getElementById("ticket-price");
+    
+
+    seat.classList.add("bg-[#1DD100]","text-white");
+    
+    console.log(seatName)
+
+         //Ticket quantity
+
+    const div = document.createElement("div");
+    div.classList.add("selected-ticket");
+    div.classList.add("flex");
+    div.classList.add("justify-between");
+    div.classList.add("px-5");
+
+    const p1 = document.createElement("p");
+    const p2 = document.createElement("p");
+    const p3 = document.createElement("p");
+
+    p1.innerText = seatName;
+    p2.innerText = "Economy";
+    p3.innerText = ticketPrice;
+
+    div.appendChild(p1);
+    div.appendChild(p2);
+    div.appendChild(p3);
+
+    ticketAll.appendChild(div);
+    ticketAll
+    console.log(div)
+
+
+
+
+   });
+    
+};
